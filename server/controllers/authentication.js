f828ade30434c1852f29ceb84b3b5598a5a2a976
M. Hassan Khurshid <mkshid@gmail.com>
2017-06-03 20:13:50 +0200

    Create a jwt token

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,12 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user){
+    const timestamp = new Date().getTime();
+    return jwt.encode({sub: user.id, iat: timestamp}, config.secret);
+}
+
 
 exports.signup = function(req, res, next){
     const email = req.body.email;
@@ -21,7 +29,7 @@ exports.signup = function(req, res, next){
         user.save(function(err){
             if (err) {return next(err);}
 
-            res.json({success: true});
+            res.json({token: tokenForUser(user)});
         });
     });
 
