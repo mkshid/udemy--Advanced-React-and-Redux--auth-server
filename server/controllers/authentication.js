ada4fa694cb4335aa22f001a711e48c62b80d386
M. Hassan Khurshid <mkshid@gmail.com>
2017-06-03 18:51:45 +0200

    Create a user if not exists

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -6,7 +6,19 @@ exports.signup = function(req, res, next){
     
 
     User.findOne({email: email}, function(err, existingUser){
-        
+        if (err){ return next(err);}
+
+        if (existingUser){
+            res.status(422).send({error: 'Email is in use'});
+        }
+
+        const user = new User({email: email, password: password});
+
+        user.save(function(err){
+            if (err) {return next(err);}
+
+            res.json({success: true});
+        });
     });
 
 }
