638a52825a9bd440d89551f4f530d9e670f8b76a
M. Hassan Khurshid <mkshid@gmail.com>
2017-07-01 15:57:20 +0200

    enable cors

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // Db setup
 
@@ -12,6 +13,7 @@ mongoose.connect('mongodb://localhost:auth/auth');
 
 // App setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
